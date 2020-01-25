16e54837355a980c26ae72ccc63a1423b3b63797
Ricky Hussmann <ricky.hussmann@gmail.com>
2020-01-25 10:32:58 -0500

    Test mailing on deployed infrastructure
    * Fix `package.json`
    * Create test endpoint

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,10 +2,14 @@ import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
 import { ApiBody, ApiBearerAuth } from "@nestjs/swagger";
+import { MailerService } from "./mailer.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailer: MailerService
+  ) {}
 
   @Get("json")
   getJson(): any {
@@ -41,5 +45,11 @@ export class AppController {
   @Get("profile")
   getProfile(@Request() req: any) {
     return req.user;
+  }
+
+  @Post("sendEmail")
+  async sendEmail() {
+    const me = "ricky.hussmann@gmail.com";
+    await this.mailer.sendEmail(me, me, "Hello", "This is an email");
   }
 }
