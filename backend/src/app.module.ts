3e5e7756a44e5a263ef7f706bfbbce648b3f2096
Ricky <rhussmann@users.noreply.github.com>
2020-01-24 22:04:29 -0500

    Add a sample endpoint authenticated with JWT (#6)
    * `/profile` endpoint
    * Uses bearer token authentication
    * Added Swagger support
    * Extend JWT lifetime to 1 day

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,15 +5,16 @@ import { UsersService } from "./users.service";
 import { LocalStrategy } from "./local.strategy";
 import { JwtModule } from "@nestjs/jwt";
 import { jwtConstants } from "./constants";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: "60s" }
+      signOptions: { expiresIn: "1d" }
     })
   ],
   controllers: [AppController],
-  providers: [AuthService, UsersService, LocalStrategy]
+  providers: [AuthService, UsersService, LocalStrategy, JwtStrategy]
 })
 export class AppModule {}
